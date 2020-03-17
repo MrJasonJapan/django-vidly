4a3357a880211c6086ea1bbbf71fd6ca43ea9e64
SpaGettys <jgettys@gmail.com>
2020-03-17 19:07:18 +0900

    Display a list of movies from the database (plain text)
    test here
    http://127.0.0.1:8000/movies/

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    # Movie.objects.filter(release_year=1985)
+    # Movie.objects.get(id=1)
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    return HttpResponse(output)
